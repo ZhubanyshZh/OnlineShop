239d7dd5e07e338ba2d0b1acb3c54b9f3241ae39
Beibit05 <beibit.muzappar@narxoz.kz>
2024-02-23 23:37:34 +0600

    Shop бинын құрдым

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,13 +1,21 @@
 package org.example.config;
 
+import org.example.Product;
+import org.example.Shoes;
 import org.example.Shop;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.ArrayList;
+
 @Configuration
-
 public class AppConfig {
-
+    @Bean("Shop")
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public Shop shop(ArrayList<Product> products){
+        Shop shop = new Shop("Men's Shop", products);
+        return shop;
+    }
 }
