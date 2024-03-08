3ff5d2c854e6c85b133a458edd57d83c8da109fd
Zhubanysh Zharylkassyn <153410943+ZhubanyshZh@users.noreply.github.com>
2024-03-08 19:13:20 +0600

    Delete del_fav.php

--- del_fav.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if($_SERVER['REQUEST_METHOD']=='GET' && isset($_SESSION['client_id'])){
-    require_once 'Database.php';
-    $id = $_GET['id'];
-    $db->delete_fav_by_prod_id($id, $_GET['client_id']);
-}
-?>
