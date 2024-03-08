a8a67538cd538339f876eb7d2900f26d89cccde3
Zhubanysh Zharylkassyn <153410943+ZhubanyshZh@users.noreply.github.com>
2024-03-08 19:13:28 +0600

    Delete delete_product.php

--- delete_product.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if($_SERVER['REQUEST_METHOD']=='POST' && $_SESSION['role'] == 'admin' && isset($_POST['id'])){
-        require_once 'Database.php';
-        $db->delete_product($_POST['id']);
-        header("Location: profile.php?deleted");
-    }
-?>
