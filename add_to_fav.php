0e538b60f727291fc3430818aa8d5fbeb59d44a6
Zhubanysh Zharylkassyn <153410943+ZhubanyshZh@users.noreply.github.com>
2024-03-08 19:12:52 +0600

    Delete add_to_fav.php

--- add_to_fav.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if($_SERVER['REQUEST_METHOD']=='GET' && isset($_SESSION['client_id'])){
-    require_once 'Database.php';
-    $id = $_GET['id'];
-    $db->set_to_fav($id, $_GET['client_id']);
-}
-?>
